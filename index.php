a237bd5929146feb5d65b9743ee801b79c0db07b
Torikku-senpai <patrickabiog11@gmail.com>
2021-06-24 20:22:46 +0800

    Basic-Calculator

---
+++ index.php
@@ -0,0 +1,64 @@
+<?php
+///all variables
+
+echo "<b>";
+echo '<p align = "center">';
+echo "Patrick Mathew L. Abiog";
+$hello = "BASIC CALCULATOR";
+$hello1 = "First number :1";
+$hello2 = "Second number :2";
+echo "<b/>";
+$space = " ";
+$space1 = " ";
+$space2 = " ";
+$space3 = " ";
+$num1 = 1;
+$num2 = 2;
+echo "<br/>"; 
+
+	
+///all output
+
+echo '<p align = "center">';
+echo $hello;
+echo "<br/>";
+echo '<p align = "left">';
+echo $space;
+echo "<br/>";
+echo '<p align = "center">';
+echo $hello1;
+echo "<br/>";
+echo $space1;
+echo "<br/>";
+echo $hello2;
+echo "<br/>";
+echo $space2;
+echo "<br/>";
+echo '<p align = "left">';
+echo "ADDITION";
+echo "<br/>";
+echo "The sum is:";
+echo $num1 + $num2;
+echo "<br/>";
+echo $space3;
+echo "<br/>";
+echo "SUBTRACTION";
+echo "<br/>";
+echo "The difference is:";
+echo $num1 - $num2;
+echo "<br/>";
+echo $space3;
+echo "<br/>";
+echo "Multiplication";
+echo "<br/>";
+echo "The product is:";
+echo $num1 * $num2;
+echo "<br/>";
+echo $space3;
+echo "<br/>";
+echo "Division";
+echo "<br/>";
+echo "The quotient is:";
+echo $num1 / $num2;
+
+?>
